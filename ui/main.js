008b0f1550198cc9b2bc28f0990e487a4bfb822d
anantajitjg <anantajitjg2010@gmail.com>
2016-09-25 16:30:13 +0530

    main.js and style.css changed

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,19 @@
 console.log('Loaded!');
 var logo=document.getElementById("logo");
-logo.style.transform="rotate(90deg)";
+var rotate;
+var angle=0;
+function logoRotate(){
+	angle=angle+45;
+	logo.style.transform="rotate("+angle+"deg)";
+	if(angle==1080){
+		clearInterval(rotate);
+	}
+}
+logo.onmouseover=function(){
+	angle=0;
+	rotate=setInterval(logoRotate,50);
+};
+logo.onmouseout=function(){
+	angle=0;
+	clearInterval(rotate);
+};
