c46d912769031787987134df938911fd72aac8d2
Anantajit <anantajitjg2010@gmail.com>
2016-09-26 19:07:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,17 @@ logo.onmouseout=function(){
 };
 //counter code goes here
 var button=document.getElementById("counter");
+var span=document.getElementById("count");
 button.onclick=function(){
   var request=new XMLHTTPRequest();
-  
+  request.onreadystatechange=function(){
+    if(request.readyState===XMLHTTPRequest.DONE){
+        if(request.status===200){
+            var counter=request.responseText;
+            span.innerHTML=counter.toString();
+        }
+    }  
+  };
+  request.open("GET","http://anantajitjg.imad.hasura-app.io/counter",true);
+  request.send();
 };
