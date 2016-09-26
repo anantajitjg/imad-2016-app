0df17e572a3a0f66b3d924266821c506b903682d
Anantajit <anantajitjg2010@gmail.com>
2016-09-26 19:12:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,9 +21,9 @@ logo.onmouseout=function(){
 var button=document.getElementById("counter");
 var span=document.getElementById("count");
 button.onclick=function(){
-  var request=new XMLHTTPRequest();
+  var request=new XMLHttpRequest();
   request.onreadystatechange=function(){
-    if(request.readyState===XMLHTTPRequest.DONE){
+    if(request.readyState===XMLHttpRequest.DONE){
         if(request.status===200){
             var counter=request.responseText;
             span.innerHTML=counter.toString();
