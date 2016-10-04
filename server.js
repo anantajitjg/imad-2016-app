872248be7f60e0f36e8f405583f91a91e2fb7b31
Anantajit <anantajitjg2010@gmail.com>
2016-10-04 12:55:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/:articleID',function(req,res){
  // res.sendFile(path.join(__dirname, 'ui/fonts/', 'glyphicons-halflings-regular.'+req.params.fontExt));
 //});
 
-var port = 8083; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8083, function () {
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
